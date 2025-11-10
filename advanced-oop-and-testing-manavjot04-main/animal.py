dd26315d8427b84dc5c381aeb01ff39407b404fa
manavjot04 <dutmy005@mymail.unisa.edu.au>
2025-11-10 20:56:43 +1030

    Added the basic diet and environment enums for animals

---
+++ advanced-oop-and-testing-manavjot04-main/animal.py
@@ -0,0 +1,23 @@
+'''
+File: filename.py
+Description: A brief description of this Python module.
+Author: Billy Bizilis
+ID: 110100110
+Username: bizvy001
+This is my own work as defined by the University's Academic Integrity Policy.
+'''
+
+from enum import Enum, auto
+
+class DietType(Enum):
+    HERBIVORE = auto()
+    CARNIVORE = auto()
+    OMNIVORE = auto()
+
+class EnvironmentType(Enum):
+    SAVANNAH = auto()
+    AQUATIC = auto()
+    RAINFOREST = auto()
+    DESERT = auto()
+    AVIARY = auto()
+    REPTILE_HOUSE = auto()
---
+++ advanced-oop-and-testing-manavjot04-main/enclosure.py
@@ -0,0 +1,8 @@
+'''
+File: filename.py
+Description: A brief description of this Python module.
+Author: Billy Bizilis
+ID: 110100110
+Username: bizvy001
+This is my own work as defined by the University's Academic Integrity Policy.
+''
